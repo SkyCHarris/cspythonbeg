b24624a25cf80177c79e50da183187c1139c47b4
Sky Harris <skyler.iqeq@gmail.com>
2023-12-08 12:23:48 -0700

    Operators, integers, floats

---
+++ integers_floats.py
@@ -0,0 +1,54 @@
+
+#! Integers and Floats - Working with Numeric Data
+
+# Numbers in python are generally working with integers and floats
+# Int: whole number
+# Float: decimal
+
+# Python has a built-in function called type, where we can see the datatype of an object
+
+num = 3
+num2 = 3.14
+
+print(type(num))
+print(type(num2))
+
+#? Arithmetic Operators:
+
+# Division /
+
+print(3/2)
+
+# Floor Division //
+
+print(3//2)
+
+# Modulu (remainders)
+
+print(3%2)
+
+# Even or Odd
+
+print(2%2)
+print(3%2)
+print(4%2)
+print(5%2)
+
+# Incrementing variables
+
+num = 1
+num = num + 1
+
+#aka
+
+num = 1
+num += 1
+
+# Round
+# Round our values to the nearest integer value
+
+print(round(3.75))
+
+# Round to certain digit
+
+print(round(3.75, 1))
