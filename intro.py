1e1d0f7deadbf8cfd8076071ac872f0341cd7f2f
Sky Harris <skyler.iqeq@gmail.com>
2023-12-07 22:18:30 -0700

    Count, find, replace, concatenate, formatted strings, f-strings

--- intro.py
+++ intro.py
@@ -44,3 +44,65 @@ print(message.lower())
 
 message = 'Hello World'
 print(message.count('l'))
+
+# Find - Index of where characters are found
+
+message = 'Hello World'
+print(message.find('World'))
+
+# Replace
+# Replace some characters in our string with other characters
+    # Returns a new string with those values replaced, not making changes to original string
+
+message = 'Hello World'
+new_message = message.replace('World', 'Universe')
+print(new_message)
+
+# Add multiple strings and concatenate them together
+    # Getting a little complicated with this syntax
+
+greeting = 'Hello'
+name = 'Michael'
+
+message = greeting + ', ' + name + '. Welcome!'
+print(message)
+
+# Formatted Strings
+# Write the sentence as it'll appear
+# Put placeholders in place of variables
+    # Everywhere we want to replace with a variable, we use a placeholder {}
+
+greeting = 'Hello'
+name = 'Michael'
+
+message = '{}, {}. Welcome!'.format(greeting, name)
+
+print(message)
+
+# F-Strings
+# Make string formatting as simple as possible
+# Write the variables directly in the placeholders
+# Can write directly in the placeholder (ex. {name.upper()})
+
+greeting = 'Hello'
+name = 'Michael'
+
+message = f'{greeting}, {name}. Welcome!'
+
+print(message)
+
+# dir()
+# Pass in a variable to get all the methods available
+
+greeting = 'Hello'
+
+print(dir(name))
+
+# help()
+# See what the methods do
+# Type a method into the help() parameter ex. -> help(str)
+
+greeting = 'Hello'
+name = 'Michael'
+
+print(help(str))
